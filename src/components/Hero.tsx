851d681058d71f8fd004536a1da6a046a20226dd
Shayan <shayanabedi.dev@gmail.com>
2023-11-13 21:00:33 -0500

    implement Hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,34 @@
+import { HiArrowNarrowRight } from 'react-icons/hi';
+
+const Hero = () => {
+  return (
+    <div className="w-full h-screen">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-[var(--green)] mb-7">Hi, my name is</p>
+        <h1 className="text-[var(--lightest-slate)] text-4xl sm:text-7xl font-bold">
+          Shayan Abedi.
+        </h1>
+        <h2 className="text-[var(--slate)] text-4xl sm:text-7xl font-bold">
+          I build things for the web.
+        </h2>
+        <p className="text-[var(--slate)] mt-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+          perferendis, aliquid a odit, ut libero maxime sunt id neque quis
+          dolorum animi? Nemo obcaecati distinctio sunt, tempore magnam illo
+          similique.
+        </p>
+        <div>
+          <button className="group flex items-center mt-10 hover:bg-red">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
